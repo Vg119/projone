bd754aea27816088bdfeb8e0cc2e1be982311365
Vg119 <90823605+Vg119@users.noreply.github.com>
2023-04-16 14:31:24 +0530

    test1

---
+++ lib/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //common data types
+    int hel = 7;double four = 9.65;
+    String two = "test",three = "three";
+    bool istrue  = true;
+    num a = 34;
+
+    var gen = "hello";
+    const constant = "hej"; //can be constant of any type
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue app"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to my  $hel house bitches !!!! $two $four"+" "+three +" $istrue"),
+
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:projone/homepage.dart';
 
-void main(){
+void main(){    //entry point of app
   runApp(MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);   // when data is passed onto app
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-          child: Center(
-            child: Container(
-              child: Text("Welcome to my house bitches !!!!"),
 
-            ),
-          ),
-        ),
+
+    return MaterialApp(
+        home: Homepage(),
     );
   }
 }
