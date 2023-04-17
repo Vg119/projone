18da9c17d136b550a771e1dfd8a25d1bef90cbd9
Vg119 <90823605+Vg119@users.noreply.github.com>
2023-04-17 13:39:12 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projone/pages/homepage.dart';
 import 'package:projone/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){    //entry point of app , not in any class
   runApp(MyApp());
@@ -19,12 +20,13 @@ class MyApp extends StatelessWidget {
         //home: Homepage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(     //Thememode.light defined
-        primarySwatch: Colors.deepOrange
+        primarySwatch: Colors.deepOrange,
+        fontFamily: GoogleFonts.lato().fontFamily,   //setting style of app texts
       ),
       darkTheme: ThemeData(    //Thememode.dark defined
         brightness: Brightness.dark
       ),
-   initialRoute: "/home",   //setting initialroute to "/home" , by default set to "/"
+  // initialRoute: "/home",   //setting initialroute to "/home" , by default set to "/"
    routes: {
           "/":(context) =>  Loginpage(),   // writing new LoginPage and LoginPage is same .
           "/home" : (context) => Homepage()
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
 class Loginpage extends StatelessWidget {
-  const Loginpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(   //puts in centre
-        child: Text("Login page",   //text
-          style: TextStyle(
-            fontStyle: FontStyle.italic,
-            fontSize: 50
-          ),),
+    return  Material(
+      color: Colors.white,      //white to match image to background
+      child: SingleChildScrollView(  //singlechildscrollview to make sure image fits in .
+        child: Column(
+
+          children: [
+            Image.asset("assets/images/login.png",height: 350), //Boxfit.cover to fit the image
+            SizedBox(height: 20),      //empty space of ht 20
+            Text("Welcome",
+            style: TextStyle(
+              fontSize: 25,
+              fontStyle: FontStyle.normal
+            ),),
+            SizedBox(height: 20), //empty space of ht 20
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:16.0,horizontal: 32.0),   //horizontal and vertical column different
+              child: Column(children: [TextFormField(   //childen are 2 textformfields
+                decoration: InputDecoration(
+                    hintText: "Enter user name",
+                    labelText: "Username"
+                ),
+              ),
+                TextFormField(
+                  obscureText : true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password"
+                  ),
+                ),
+                SizedBox(height: 20),
+              ElevatedButton(
+                child: Text("LOGIN"),
+                style: TextButton.styleFrom(),
+                onPressed: (){
+                  print("hello vergab");
+                },
+              )
+              ],),
+            )
+          ],
+
+        ),
       ),
     );
   }
