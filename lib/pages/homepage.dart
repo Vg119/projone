76396ed413f26990bb7611bac6c0ad072d26f9ff
Vg119 <90823605+Vg119@users.noreply.github.com>
2023-04-24 20:52:27 +0530

    day 11 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,13 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:projone/Catalog.dart';
 import 'package:projone/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {   //we make homepage a stateful widget as we have to get info from catalog.json and get info and update homepage
   const Homepage({Key? key}) : super(key: key);
 
   @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {     //this init is called before build is called
+    // TODO: implement initState
+    super.initState();
+    loaddata();
+  }
+  loaddata() async
+  {
+    var catalog_json_data = await rootBundle.loadString("assets/files/Catalog.json");      //async-await coz this takes time load(returns Future type of data.
+    var decoded_data = jsonDecode(catalog_json_data); // the data in catalog_json_data is  String . jsonDecodeconvert from String to map(dynamic) ; jsonEncode does the reverse
+    var data = decoded_data["products"]; //to get only items with key "products" from decoded_data.
+   // print(decoded_data);
+  }
+
   Widget build(BuildContext context) {
     //common data types
     int hel = 7;double four = 9.65;
